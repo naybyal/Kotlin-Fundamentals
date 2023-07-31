93af30cf2bd2a70aed172d10e342c9fd6730eff5
naybyal <officiallynabiel@gmail.com>
2023-07-31 18:25:11 +0530

    Learnt about variables and mathematical operations

--- src/main/kotlin/MathematicalOperations.kt
+++ src/main/kotlin/MathematicalOperations.kt
@@ -2,6 +2,37 @@ fun main(args: Array<String>) {
     var x = 5
     var y = 3
 
-    val result = x+y
+    var result = x+y
     println("result = $result")
+//    another way of doing this :
+    println("sum = ${x+y}")  // just like in BASH // ADD
+    println("difference = ${x-y}")  // SUBTRACT
+    println("product = ${x*y}")  // MULTIPLY
+    println("quotient = ${x/y}")  // DIVIDE
+    println("remainder = ${x%y}") // MODULO DIVISION
+    println()
+
+    result += 2 //  result = result + 2
+    println("+=2 updated result =  $result")
+
+    result *= 2 //  result = result * 2
+    println("*=2 updated result =  $result")
+
+    result -= 2 //  result = result - 2
+    println("-=2 updated result =  $result")
+
+    result /= 2 //  result = result / 2
+    println("/=2 updated result =  $result")
+
+    result %= 2 //  result = result % 2
+    println("%=2 updated result =  $result")
+
+    x=0
+    println()
+    println("x = $x")
+    println("x++ = ${x++}")
+    println("++x = ${++x}")
+    println("x-- = ${x--}")
+    println("--x = ${--x}")
+
 }
