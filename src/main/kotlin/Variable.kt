564135cb4e274cac17365ff2a362a188ca4d8df9
naybyal <officiallynabiel@gmail.com>
2023-07-31 15:09:04 +0530

    Initial commit.

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,15 @@
+//  basics for variables
+
+fun main() {
+    val userName: String = "Rosetta"
+    var x: Float = 2.001F
+    println("Hello $userName!")
+    println(x)
+//    byte short int long
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+    println("Integer : Max=$maxIntegerValue\tMin=$minIntegerValue")
+
+
+    var byteVariable: Byte = 2
+}
