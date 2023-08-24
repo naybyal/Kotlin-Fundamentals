a3c4f2627df3443d7ac66a2e65743d9e6eee3061
naybyal <officiallynabiel@gmail.com>
2023-08-24 21:02:34 +0530

    OOP : Learnt some more.

---
+++ src/main/kotlin/OOPAbstract.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+}
+
+abstract class VehicleNew {
+    //  can only be inherited by other classes....
+    //  similar to interface...
+    val text = "some text"  // in interface, we cannot do this.
+    abstract fun move();
+    abstract fun stop();
+}
+// we can only inherit from a single class.. but inherit from many interfaces (DIAMOND PROBLEM just like in Java)
+class NewCar(var name: String, var color: String, val engines: Int, val doors: Int): Vehicle() {
+    override fun move() {
+        super.move()
+        println("$name is moving")
+    }
+
+    override fun stop() {
+        super.stop()
+        println("$name has stopped")
+    }
+}
